7fa835508aa99eeffe8eca7b432f2d19f2d35f35
Jonathan Zier <jzier3@gmail.com>
2018-09-16 22:06:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    //The exercises that will be graded in Homework 3 are 3.1, 3.2, 3.5, 4.1, 4.3, 5.1, 5.2, 5.5, 5.7, 6.1, and 6.4.
+}
